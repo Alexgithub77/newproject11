c2e23d42f387a120a88c89e0cfbc5bb3b3e66dab
Alexgithub97 <alexgithub77@gmail.com>
2019-09-12 14:31:57 +0530

    newcode

---
+++ src/gittrytest10/Gittrytest10.java
@@ -0,0 +1,21 @@
+package gittrytest10;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Gittrytest10 {
+	
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "F:\\Alex\\WorkFolder\\Gittry10\\lib\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		driver.manage().window().maximize();
+		
+	}
+
+}
